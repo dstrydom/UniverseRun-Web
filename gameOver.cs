ddf19b6dc60d02778bd5fe54fcf8b0ab79cf60c7
Danelze "Danny" Strydom <dstry002@ucr.edu>
2014-06-26 21:32:40 -0700

    Create gameOver.cs

---
+++ gameOver.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class gameOver : MonoBehaviour {
+
+	//---------------Play Again/Quit Game---------------------
+	public bool isQuit = false;
+	
+	void OnMouseEnter() {
+		renderer.material.color = Color.red;
+	}
+	
+	void OnMouseExit() {
+		Screen.showCursor = true;
+		renderer.material.color = Color.white;
+	}
+	
+	void OnMouseDown() {
+		if(isQuit) {
+			//Application.Quit();
+			Application.ExternalEval("window.close()"); //added for web
+		}
+		else {
+			Screen.showCursor = false;
+			Application.LoadLevel(1);
+		}
+	}
+
+	//----------------------Display Score-------------------------------
+	void OnGUI() {
+		Screen.showCursor = true;
+	}
+}

fe1aef33784c776e20040ba28870e25682d15646
Danelze "Danny" Strydom <dstry002@ucr.edu>
2014-06-26 21:34:12 -0700

    Create MenuScript.cs

---
+++ MenuScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour 
+{
+	public bool isQuit = false;
+
+	void OnMouseEnter() {
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit() {
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseDown() {
+		if(isQuit) {
+			//Application.Quit();
+			Application.ExternalEval("window.close()"); //added for web
+		}
+		else {
+			Screen.showCursor = false;
+			Application.LoadLevel(1);
+		}
+	}
+}
